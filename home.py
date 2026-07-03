d292bdb4dda92f16276ad72c0528e423f2bf993f
TylerK4Brown <tylerk4brown@gmail.com>
2026-07-03 02:48:15 -0400

    Initial page layout in preparation for MVP #1

---
+++ home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="1-2-3 Point Play",
+    layout="wide"
+)
+
+pages = {
+    "Welcome to 1-2-3 Point Play!": [
+        st.Page("pages/landing_page.py"),
+    ],
+    "Make Your Picks!": [
+        st.Page("pages/make_your_picks.py"),
+    ]
+}
+
+pg = st.navigation(pages, position="hidden")
+pg.run()
---
+++ pages/landing_page.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import streamlit_extras as stx
+
+st.title("Welcome to 1-2-3 Point Play!", text_alignment="center")

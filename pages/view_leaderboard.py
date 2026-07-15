b71594ed5fb10389f3f2960a6968807fa4f021c0
TylerK4Brown <tylerk4brown@gmail.com>
2026-07-14 20:26:54 -0400

    Drafted implementation of the leaderboard page

--- pages/view_leaderboard.py
+++ pages/view_leaderboard.py
@@ -1,5 +1,28 @@
 import streamlit as st
+from database_operations.database import get_all_user_picks, get_user_points
 
 st.markdown("# Current leaderboard", text_alignment="center")
-st.markdown("Work in progress :smile:", text_alignment="center")
 st.divider(width='stretch')
+
+get_user_points()  # Call the function to retrieve and store user points in session state
+
+points_data = {
+    "Dad": st.session_state.get("Dad_accumulated_points"),
+    "TJ": st.session_state.get("TJ_accumulated_points"),
+    "Tyler": st.session_state.get("Tyler_accumulated_points")
+}
+
+# Creates a tuple, takes index 1 (points value) from the tuple for each item in the dictionary, sorts the dictionary by that value, and returns a new dictionary with the sorted values
+points_data_sorted = dict(sorted(points_data.items(), key=lambda item: item[1], reverse=True))
+
+# enumerate makes it so we can get the index of the items stored in the dictionary
+# this is paired with the medals in the list above
+medals = ["🥇", "🥈", "🥉"]
+for index, (name, points) in enumerate(points_data_sorted.items()):
+    medal = f"{medals[index]}"
+    st.markdown(f"# {medal}**{name}**: {points} points", text_alignment="center")
+
+col1, col2, col3 = st.columns([1, 1, 1])
+with col2:
+    if st.button("Return to Landing Page", width=700, key="return_landing_page"):
+        st.switch_page("pages/landing_page.py")

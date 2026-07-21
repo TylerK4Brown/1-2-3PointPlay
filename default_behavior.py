c8f6a577ff455bfc10b2539b82c31ee9a6e839f6
TylerK4Brown <tylerk4brown@gmail.com>
2026-07-20 20:13:33 -0400

    Edit column definition syntax

--- default_behavior.py
+++ default_behavior.py
@@ -7,7 +7,7 @@ def check_session_states():
     # Check if the session states for name and point_picks exist, if not, redirect to landing page
     if "name" not in st.session_state or "point_picks" not in st.session_state:
         st.title("NAME NOT SELECTED - PLEASE RETURN TO THE LANDING PAGE AND SELECT A NAME", text_alignment="center")
-        col1, col2, col3 = st.columns([1, 1, 1])
+        col1, col2, col3 = st.columns(3)
         with col2:
             if st.button("Return to Landing Page", width=700, key="return_landing_page"):
                 st.switch_page("pages/landing_page.py")

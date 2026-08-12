f419c40f147f10db69500831ba95c76de5c3dc73
TylerK4Brown <tylerk4brown@gmail.com>
2026-08-12 19:17:31 -0400

    Begun drafting header comments for all files to ensure consistency and clarity.

--- database_operations/database.py
+++ database_operations/database.py
@@ -1,9 +1,18 @@
-# Module that handles all database operations across the application
-# Makes it easier to track where DB interactions happen throughout the program
+# database.py
+# All database interactions are handled in this file, assisting with many repetitive database operations throughout the application
+# Separating this file out into one module allows for easier maintenance and debugging of database operations
 
 from st_supabase_connection import SupabaseConnection
 import streamlit as st
 
+# ======== CREATE OPERATIONS ==========
+# Create a new user entry in the database if the name does not exist yet
+# This will only be used at the start of the season to create initial entries for each user
+def create_user_db_entry(data_entry):
+    conn = st.connection("user_picks", type=SupabaseConnection)
+    conn.table("user_picks").insert(data_entry).execute()
+
+# ======== GET OPERATIONS ==========
 # Get all user picks
 # Select * from the user_picks table and return the rows
 def get_all_user_picks():
@@ -17,12 +26,8 @@ def get_user_picks(name):
     row = conn.table("user_picks").select("*").eq("name", name).execute().data
     return row
 
-# Create a new user entry in the database if the name does not exist yet
-# This will only be used at the start of the season to create initial entries for each user
-def create_user_db_entry(data_entry):
-    conn = st.connection("user_picks", type=SupabaseConnection)
-    conn.table("user_picks").insert(data_entry).execute()
-
+# Get the picks from a specific week for a specific user
+# Used to find historical picks for a specific week
 def get_picks_by_week(week_number, name):
     week = f"week_{week_number}"
     conn = st.connection("user_picks", type=SupabaseConnection)
@@ -40,37 +45,15 @@ def get_point_total_for_week(week_number, name):
     point_total = row[0][point_total_col_name]
     return point_total
 
-# Update the user picks in the database for a specific user
-def update_user_picks(name, data_entry):
-    conn = st.connection("user_picks", type=SupabaseConnection)
-    conn.table("user_picks").update(data_entry).eq("name", name).execute()
-
-# Update points for a specific user in the database
-# Called after user clicks on the "view_player_picks" page
-# Updates point totals based on the live score of the game
-# Also updates the home and away team score for that game, plus the covering status
-def update_user_points(name, current_week_total, accumulated_points):
-    conn = st.connection("user_picks", type=SupabaseConnection)
-    conn.table("user_picks").update({
-            "current_week_total": current_week_total, 
-            "accumulated_points": accumulated_points
-        }).eq("name", name).execute()
-
-def update_win_loss_info(name, correct_picks, incorrect_picks, push_picks):
-    conn = st.connection("user_picks", type=SupabaseConnection)
-    win_loss_json = {
-        "picks_correct": correct_picks,
-        "picks_incorrect": incorrect_picks,
-        "picks_push": push_picks
-    }
-    conn.table("user_picks").update({"win_loss_record": win_loss_json}).eq("name", name).execute()
-
+# Get the player's win/loss record for a specific week from the database
+# Used in the pick history page to display the player's win/loss record from each week
 def get_win_loss_by_week(week_number, name):
     conn = st.connection("user_picks", type=SupabaseConnection)
     row = conn.table("user_picks").select(f"win_loss_week_{week_number}").eq("name", name).execute().data
     win_loss = row[0][f"win_loss_week_{week_number}"]
     return win_loss
 
+# Get the player's current win/loss record from the database
 def get_win_loss_record(name):
     conn = st.connection("user_picks", type=SupabaseConnection)
     row = conn.table("user_picks").select("win_loss_record").eq("name", name).execute().data
@@ -88,6 +71,41 @@ def get_user_points():
             accumulated_points = row[0]["accumulated_points"]
             st.session_state[f"{name}_accumulated_points"] = accumulated_points
 
+# Get the current week number from the database
+def get_week_number():
+    conn = st.connection("user_picks", type=SupabaseConnection)
+    row = conn.table("user_picks").select("current_week").eq("name", "Tyler").execute().data
+    current_week_number = row[0]["current_week"]
+    return current_week_number
+
+# ======== UPDATE OPERATIONS ==========
+# Update the picks in the database for a specific user
+def update_user_picks(name, data_entry):
+    conn = st.connection("user_picks", type=SupabaseConnection)
+    conn.table("user_picks").update(data_entry).eq("name", name).execute()
+
+# Update points for a specific user in the database
+# Called after user clicks on the "view_player_picks" page
+# Updates point totals based on the live score of the game
+# Also updates the home and away team score for that game
+def update_user_points(name, current_week_total, accumulated_points):
+    conn = st.connection("user_picks", type=SupabaseConnection)
+    conn.table("user_picks").update({
+            "current_week_total": current_week_total, 
+            "accumulated_points": accumulated_points
+        }).eq("name", name).execute()
+
+# Update win/loss information after loading the "view_player_picks" page
+# Stores any differences that may have occurred in the win/loss record for the current week in the database
+def update_win_loss_info(name, correct_picks, incorrect_picks, push_picks):
+    conn = st.connection("user_picks", type=SupabaseConnection)
+    win_loss_json = {
+        "picks_correct": correct_picks,
+        "picks_incorrect": incorrect_picks,
+        "picks_push": push_picks
+    }
+    conn.table("user_picks").update({"win_loss_record": win_loss_json}).eq("name", name).execute()
+
 # Update the score of a specific pick for a specific user in the database
 # Called after clicking on the "view_player_picks" page, after the API call is made to get the live scores for each game
 def update_scores(name, score_home_team, score_away_team, iteration_index, sorted_picks, covering_spread):
@@ -104,10 +122,3 @@ def update_scores(name, score_home_team, score_away_team, iteration_index, sorte
                 "picks": sorted_picks
             }
         }).eq("name", name).execute()
-
-# Get the current week number from the database
-def get_week_number():
-    conn = st.connection("user_picks", type=SupabaseConnection)
-    row = conn.table("user_picks").select("current_week").eq("name", "Tyler").execute().data
-    current_week_number = row[0]["current_week"]
-    return current_week_number

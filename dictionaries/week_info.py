d6b277afe65cef6c3072b69a96901e3b48e839d4
TylerK4Brown <tylerk4brown@gmail.com>
2026-09-01 22:39:11 -0400

    Change back to preseason dates and times

--- dictionaries/week_info.py
+++ dictionaries/week_info.py
@@ -3,75 +3,15 @@
 
 NFL_2026_WEEK_TIME_WINDOWS = {
 	1: {
-		"commence_timeFrom": "2026-09-10T00:00:00Z",
-		"commence_timeTo": "2026-09-16T00:00:00Z",
+		"commence_timeFrom": "2026-08-12T00:00:00Z",
+		"commence_timeTo": "2026-08-18T00:00:00Z",
 	},
 	2: {
-		"commence_timeFrom": "2026-09-17T00:00:00Z",
-		"commence_timeTo": "2026-09-23T00:00:00Z",
+		"commence_timeFrom": "2026-08-19T00:00:00Z",
+		"commence_timeTo": "2026-08-25T00:00:00Z",
 	},
 	3: {
-		"commence_timeFrom": "2026-09-24T00:00:00Z",
-		"commence_timeTo": "2026-09-30T00:00:00Z",
-	},
-	4: {
-		"commence_timeFrom": "2026-10-01T00:00:00Z",
-		"commence_timeTo": "2026-10-07T00:00:00Z",
-	},
-	5: {
-		"commence_timeFrom": "2026-10-08T00:00:00Z",
-		"commence_timeTo": "2026-10-14T00:00:00Z",
-	},
-	6: {
-		"commence_timeFrom": "2026-10-15T00:00:00Z",
-		"commence_timeTo": "2026-10-21T00:00:00Z",
-	},
-	7: {
-		"commence_timeFrom": "2026-10-22T00:00:00Z",
-		"commence_timeTo": "2026-10-28T00:00:00Z",
-	},
-	8: {
-		"commence_timeFrom": "2026-10-29T00:00:00Z",
-		"commence_timeTo": "2026-11-04T00:00:00Z",
-	},
-	9: {
-		"commence_timeFrom": "2026-11-05T00:00:00Z",
-		"commence_timeTo": "2026-11-11T00:00:00Z",
-	},
-	10: {
-		"commence_timeFrom": "2026-11-12T00:00:00Z",
-		"commence_timeTo": "2026-11-18T00:00:00Z",
-	},
-	11: {
-		"commence_timeFrom": "2026-11-19T00:00:00Z",
-		"commence_timeTo": "2026-11-25T00:00:00Z",
-	},
-	12: {
-		"commence_timeFrom": "2026-11-26T00:00:00Z",
-		"commence_timeTo": "2026-12-02T00:00:00Z",
-	},
-	13: {
-		"commence_timeFrom": "2026-12-03T00:00:00Z",
-		"commence_timeTo": "2026-12-09T00:00:00Z",
-	},
-	14: {
-		"commence_timeFrom": "2026-12-10T00:00:00Z",
-		"commence_timeTo": "2026-12-16T00:00:00Z",
-	},
-	15: {
-		"commence_timeFrom": "2026-12-17T00:00:00Z",
-		"commence_timeTo": "2026-12-23T00:00:00Z",
-	},
-	16: {
-		"commence_timeFrom": "2026-12-24T00:00:00Z",
-		"commence_timeTo": "2026-12-30T00:00:00Z",
-	},
-	17: {
-		"commence_timeFrom": "2026-12-31T00:00:00Z",
-		"commence_timeTo": "2027-01-06T00:00:00Z",
-	},
-	18: {
-		"commence_timeFrom": "2027-01-07T00:00:00Z",
-		"commence_timeTo": "2027-01-13T00:00:00Z",
-	},
+		"commence_timeFrom": "2026-08-26T00:00:00Z",
+		"commence_timeTo": "2026-09-01T00:00:00Z",
+	}
 }
